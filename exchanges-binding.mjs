bf1f3b50b71f959b41534c7328ce02b23f3554d0
Rafael Freitas Kremser <rafaelfreitaskermser@gmail.com>
2024-05-21 13:38:26 -0300

    feat: create queue bindings

---
+++ exchanges-binding.mjs
@@ -0,0 +1,48 @@
+import amqp from "amqplib";
+import { randomUUID } from "crypto";
+
+async function exchangeBinding() {
+    const conn = await amqp.connect({
+        hostname: "localhost",
+        port: 5672,
+        username: "rabbitmq",
+        password: "rabbitmq",
+        vhost: 'fanout',
+    });
+
+    const channel = await conn.createChannel();
+
+    await channel.assertExchange('notify_headers', 'headers');
+    await channel.assertQueue('email_notification');
+    await channel.assertQueue('sms_notification');
+    await channel.assertQueue('push_notification');
+
+    await channel.bindQueue('email_notification', 'notify_headers', '', {
+        'notification_type': 'email',
+    });
+    await channel.bindQueue('sms_notification', 'notify_headers', '', {
+        'notification_type': 'sms',
+    });
+    await channel.bindQueue('push_notification', 'notify_headers', '', {
+        'notification_type': 'push',
+    });
+
+    await channel.assertExchange('notify_fanout', 'fanout');
+    await channel.bindQueue('email_notification', 'notify_fanout');
+    await channel.bindQueue('sms_notification', 'notify_fanout')
+    await channel.bindQueue('push_notification', 'notify_fanout')
+    await channel.bindExchange('notify_fanout', 'notify_headers', '', {
+        'notification_type': 'all',
+    })
+
+    channel.publish('notify_headers', '', Buffer.from('meu header'), {
+        headers: {
+            notification_type: 'all'
+        }
+    })
+
+    await channel.close();
+    await conn.close();
+}
+
+exchangeBinding();
