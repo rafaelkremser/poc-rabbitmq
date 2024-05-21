33eebee9abccbc5688523072380c68d66d940e54
Rafael Freitas Kremser <rafaelfreitaskermser@gmail.com>
2024-05-21 11:25:07 -0300

    feat: learning topic

---
+++ exchanges-topic.mjs
@@ -0,0 +1,27 @@
+import amqp from "amqplib";
+
+async function exchangeTopic() {
+    const conn = await amqp.connect({
+        hostname: "localhost",
+        port: 5672,
+        username: "rabbitmq",
+        password: "rabbitmq",
+    });
+
+    const channel = await conn.createChannel();
+
+    await channel.assertExchange('system_exchange', 'topic');
+    await channel.assertQueue('system_logs');
+    await channel.assertQueue('system_errors');
+
+    await channel.bindQueue('system_logs', 'system_exchange', 'logs.#');
+    await channel.bindQueue('system_errors', 'system_exchange', '#.erro');
+
+    channel.publish('system_exchange', 'logs.system.info', Buffer.from('Sistema iniciado'));
+    channel.publish('system_exchange', 'logs.system.erro', Buffer.from('Erro no sistema'));
+
+    channel.close();
+    conn.close();
+}
+
+exchangeTopic();
